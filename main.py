e6f817a8d766409b6d63d95d8b5a0276387001c6
Valerii_Panov <val250640@mail.ru>
2023-02-06 14:38:35 +1000

    new_comments_and_personal_answer

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import datetime as dt
 import logging
 import random
 from aiogram import Bot, Dispatcher, executor, types
-from os import path
 from img_count import *
 
 # 'UTF-8-sig'
@@ -12,7 +11,7 @@ logging.basicConfig(level=logging.INFO, filename="bot_log.csv", filemode="w",
 
 MSG = "{}, Жмякни кнопку внизу, и я скину тебе мою фотку=)"
 
-bot = Bot("")
+bot = Bot("6012678486:AAEkMXJOmBjStU1kbadh6yuTBhDIdkoh7oo")
 dp = Dispatcher(bot=bot)
 img_count = counter()
 
@@ -45,7 +44,6 @@ async def photo_handler(message: types.Message):
                "Собираю пожертвование мандаринами", "Мечтаю о новой игрушке", "Когда-то я был щеночком",
                "Да-да, меня все любят", "Подумываю об актёрской карьере", "Я рыжая морда"]
     ans = random.randint(0, len(answers) - 1)
-    # count_img = counter()
     num = random.randint(0, img_count)
     name = f'photo\{num}.jpg'
     print(f'{dt.datetime.now()}\t{message.from_user.id}\t{message.from_user.full_name}'
@@ -59,31 +57,31 @@ async def photo_handler(message: types.Message):
 @dp.message_handler(content_types=['photo'])
 async def handle_docs_photo(message):
     global img_count
+    flag = False
+    answers = ['М-м-м, да я красавчик!', 'Чёрт, как я хорош!', 'Просто лучший!',
+               'Говорят, телефон высасывает душу.\nЧто ж, пусть высосет из меня всю=)',
+               'А-у-у-у-у-у!']
+    ans = random.randint(0, len(answers) - 1)
     chatId = message.chat.id
     check = {413687869: "Валера", 582499322: "Таня", 493072257: "Паша", 562407291: "Тома"}
     if chatId in check.keys():
-        # newPhoto = counter()
         print(f'{dt.datetime.now()}\t{chatId}\t{message.chat.full_name}\tnew photo {img_count}.jpg')
         loger((dt.datetime.now(), chatId, message.chat.full_name, 'Take'))
-        # await message.photo[-1].download(f'photo\{img_count}.jpg')
+        flag = True
         await message.photo[-1].download(f'photo\{counting()}.jpg')
-        await bot.send_message(message.from_user.id, 'М-м-м, да я красавчик!')
     else:
         print(f'{dt.datetime.now()}\t{chatId}\t{message.from_user.full_name}\tTrying to send a photo')
         loger((dt.datetime.now(), chatId, message.chat.full_name, 'Trying send'))
+    if flag:
+        await bot.send_message(message.from_user.id, answers[ans])
+        await bot.send_message(message.from_user.id, f'Спасибо, {check[chatId]}')
+        await bot.send_message(message.from_user.id, 'Есть ещё?)')
 
 
 def counting():
     global img_count
     img_count += 1
     return img_count - 1
-
-
-# def counter():
-#     count = 0
-#     while path.isfile(f'photo\{count}.jpg'):
-#         count += 1
-#     return count
 
 
 def loger(data):
